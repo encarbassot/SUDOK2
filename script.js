2b323546702507a1b6c6472e286c30922979fa45
Eloi Fàbrega Grech <41945355+encarbassot@users.noreply.github.com>
2025-08-24 09:10:01 +0200

    initial

---
+++ script.js
@@ -0,0 +1,109 @@
+import ElioCanvas from "./elioCanvas.js"
+import Sudoku from "./sudoku.js"
+
+
+const divCv = document.querySelector(".canvas")
+const numBtns = document.querySelectorAll(".actions button.num-btn") 
+const btnFullScreen = document.querySelector(".actions button#fullScreen") 
+
+
+let sudoku = null
+let numSelected = null
+const M = 3 // SUDOKU SPECIAL NUMBER
+const N = M*M // 9
+const W = Math.min(window.innerWidth-6,600) //Width
+const cv = new ElioCanvas(W,W,divCv)
+
+
+cv.setup = function (){
+    cv.noStroke()
+    fetchSudoku()
+}
+
+cv.draw = function (){
+
+    //background
+    cv.background("#FAFAFA")
+
+    if(!sudoku) return
+    
+    sudoku.render(cv)
+
+    // console.log(sudoku.selected)
+}
+
+cv.mouseMoved = function (e,mouseX,mouseY){
+    const col = Math.floor(mouseX/W*N)
+    const row = Math.floor(mouseY/W*N)
+    sudoku.setHover(col,row)
+    
+}
+
+cv.mouseClicked = function (e,mouseX,mouseY){
+    const col = Math.floor(mouseX/W*N)
+    const row = Math.floor(mouseY/W*N)
+    console.log("CLICK",col,row)
+    const {selectedNum} = sudoku.click(col,row,numSelected)
+
+    selectNum(selectedNum)
+    
+}
+
+//this triggers the signal to run setup and draw
+cv.start()
+
+
+
+
+
+async function fetchSudoku(){
+    const api = "https://sudoku-api.vercel.app/api/dosuku"
+
+
+    const response = await fetch(api)
+    const data = await response.json()
+
+    // console.log(data)
+    const {value, solution} = data.newboard.grids[0]
+
+    sudoku = new Sudoku(value, solution,N,M,W)
+
+    // console.log(sudoku.solution)
+}
+
+
+
+
+function selectNum(n){
+    
+    if(isNaN(n)){
+        numSelected = null
+        for(let i=0; i<N;i++){
+            numBtns[i].classList.remove("active")
+        }
+        return
+    }
+
+    const isSame = numSelected === n
+    numSelected = isSame ? null : n
+
+    for(let i=0; i<N;i++){
+        if(n-1 === i && !isSame){
+            numBtns[i].classList.add("active")
+        }else{
+            numBtns[i].classList.remove("active")
+        }
+    }
+}
+
+// -------- BIND BUTTONS --------
+numBtns.forEach((x,i)=>x.addEventListener("click",()=>{
+    selectNum(i+1)
+}))
+btnFullScreen.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
+  }
+})
---
+++ sudoku.js
@@ -0,0 +1,178 @@
+export default class Sudoku {
+
+    constructor(board, solution,N,M,W){
+
+        this.initial = board.map(row => [...row])
+        this.board = board.map(row => [...row])
+        this.solution = solution.map(row => [...row])
+
+        this.N = N
+        this.M = M
+        this.W = W
+
+        this.hover=null
+        this.error=null //={x:0,y:0}
+        this.errors = 0
+
+
+        //configs
+        this.showSameNumbers = true //same numbers as the selected will be highlighted
+        this.showRowsAndCols = true //row and column of the selected number will be higilighted
+    }
+
+
+
+    render(cv){
+        let hoverX, hoverY
+        if(this.hover){
+            hoverX = this.hover.x
+            hoverY = this.hover.y
+        }
+        
+        const nSelected = this.selected ? this.board[this.selected.y][this.selected.x] : null
+        
+        //rows
+        for(let i=0; i<this.board.length; i++){
+            const row = this.board[i]
+            const isRowSelected = this.selected?.y === i
+
+            //columns
+            for(let j=0; j<row.length; j++){
+                const n = row[j]
+                const isOriginal = n === this.initial[i][j]
+                const isHover = this.hover && hoverX === j && hoverY === i 
+                const isColSelected = this.selected?.x === j
+                const isErr = this.error && this.error.x === j && this.error.y === i
+                const isSameNAsSelected = nSelected && nSelected === n
+
+                //Cell origins
+                const w = this.W / this.N
+                const x = w*j
+                const y = w*i
+                
+                let bg = null
+                let textFill = isOriginal ? "#111" : "#999"
+                //draw cell background
+                if(isHover){
+                    //ESTA CLICKADA
+                    bg = "#000"
+                }else if((isRowSelected && isColSelected)){
+                    bg = "#222"
+                }else if(this.showRowsAndCols && (isRowSelected || isColSelected)){
+                    //ROW COLUMN SELECTED
+                    bg = "#666"
+                }else if(this.showSameNumbers && isSameNAsSelected){
+                    bg=isOriginal ? "#111" : "#444"
+                }
+
+                if(bg){
+                    cv.fill(bg)
+                    cv.rect(x,y,w,w)
+                }
+
+                if(isErr){
+                    const p = w * 0.1
+                    cv.stroke("#F88")
+                    cv.strokeWeight(15)
+                    cv.line(x+p,y+p,x+w-p,y+w-p)
+                    cv.line(x+w-p,y+p,x+p,y+w-p)
+                    cv.strokeWeight(1)
+                    cv.stroke("#000")
+                }
+                
+                if(n===0) continue
+
+
+                //color del texto
+                if(isHover || (isRowSelected && isColSelected) ){
+                    textFill = "#FFF"
+
+                }else if(this.showRowsAndCols && (isRowSelected || isColSelected)){
+                    //selected
+                    textFill = isOriginal ? "#FFF" : "#BBB"
+
+                }else if(this.showSameNumbers && isSameNAsSelected){
+                    textFill = isOriginal ? "#BBB" : "#DDD"
+                }
+
+                //TEXT
+                cv.fill(textFill)
+                cv.text(
+                    n,
+                    x+ this.W/this.N*0.3, //compensate text center
+                    y+ this.W/this.N*0.75,
+                    this.W/10
+                )
+            }   
+        }
+
+        this.renderLines(cv)
+    }
+
+    renderLines(cv){
+        // render columns
+        for(let i=1; i<this.N; i++){
+            
+            if(i%this.M === 0){
+                cv.strokeWeight(3)
+            }else{
+                cv.strokeWeight(1)
+            }
+            
+            cv.line(this.W/this.N*i,0,this.W/this.N*i,this.W)
+
+
+
+        }
+        
+        //render rows
+        for(let j=1; j<this.N; j++){
+            if(j%this.M === 0){
+                cv.strokeWeight(3)
+            }else{
+                cv.strokeWeight(1)
+            }
+            
+            cv.line(0,this.W/this.N*j,this.W,this.W/this.N*j)
+
+        }
+
+    }
+
+    setHover(col,row){
+        this.hover = {x:col,y:row}
+    }
+
+    click(col,row,n){
+        let selectedBtn = null
+
+        if(this.error) this.error = null
+
+        this.hover = {x:col,y:row}
+        if(this.selected?.x === col && this.selected?.y === row){
+            this.selected = null
+        }else{
+            this.selected =  {x:col,y:row}
+        }
+
+        if(n){
+            const isEditable = this.initial[row][col] === 0
+            if(isEditable){
+                const isCorrect = n === this.solution[row][col]
+                if(isCorrect){
+                    this.board[row][col] = n
+                }else{
+                    this.error={x:col,y:row}
+                }
+            }
+        }
+
+
+        return {
+            selectedBtn
+        }
+    }
+
+
+    
+}
